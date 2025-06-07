3ea566609fefc9997d6d2cf948f25fd7cd08f7a6
invencaosts <gui.inven@gmail.com>
2025-06-07 16:54:16 -0300

    criando tela de histórico

--- trancaduraweb_react/src/app/historico/page.tsx
+++ trancaduraweb_react/src/app/historico/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
 import Header from "@/components/Header";
+import HistoryCard from "@/components/HistoryCard";
+
+import { FaRegClock } from "react-icons/fa6";
+import Select from "react-select";
+
+const opcoes = [
+  { value: "dia", label: "Dia" },
+  { value: "semana", label: "Semana" },
+  { value: "mes", label: "Mês" },
+  { value: "ano", label: "Ano" },
+];
 
 export default function Historico() {
   return (
@@ -6,7 +19,27 @@ export default function Historico() {
       <header>
         <Header />
       </header>
-      <h1 className="text-6xl text-black">HELLO WORLD! (Histórico)</h1>
+      <section className="p-4">
+        <div>
+          <div className="p-4 w-full flex items-start justify-center flex-col gap-2 bg-white rounded-lg shadow-md border border-gray-200">
+            <div className="flex items-center justify-between w-full">
+              <div>
+                <div className="flex gap-2 items-center">
+                  <FaRegClock className="w-6 h-6 text-foreground font-bold" />
+                  <p className="text-2xl font-bold">Histórico de Operações</p>
+                </div>
+                <div className="text-gray-600 text-lg">
+                  <p>Todas as operações registradas</p>
+                </div>
+              </div>
+              <div>
+                <Select options={opcoes} placeholder="Selecione..." />
+              </div>
+            </div>
+            <HistoryCard />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
