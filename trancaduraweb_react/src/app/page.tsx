264232864cf3be2bdf4149ecc8f50c2447395093
invencaosts <gui.inven@gmail.com>
2025-06-07 18:02:42 -0300

    atualizando home page

--- trancaduraweb_react/src/app/page.tsx
+++ trancaduraweb_react/src/app/page.tsx
@@ -2,6 +2,7 @@ import HistoryCard from "@/components/HistoryCard";
 import Header from "@/components/Header";
 
 import { FaRegClock } from "react-icons/fa6";
+import { IoLockClosedOutline } from "react-icons/io5";
 
 export default function Home() {
   return (
@@ -9,31 +10,40 @@ export default function Home() {
       <header>
         <Header />
       </header>
-      <section>
-        <div>
-          <h1 className="text-4xl flex items-center justify-center font-bold">Bem-vindo, Administrador</h1>
-          <h3 className="text-3x1 text-gray-700 flex items-center justify-center">Use o botão abaixo para controlar a tranca</h3>
-          <div className="w-80 bg-white rounded-xl p-6 shadow-md mx-auto mt-12 text-center">
-            <button className="w-40 h-40 bg-black rounded-full mx-auto flex items-center justify-center text-white text-lg font-semibold">
-              Abrir Tranca
+      <section className="p-4">
+        <div className="w-full mx-auto max-w-4xl p-4 flex items-center justify-center flex-col gap-1">
+          <h1 className="font-bold text-2xl sm:text-4xl">Bem vindo, usuário</h1>
+          <p className="font-bold text-base sm:text-lg md:text-xl text-gray-600">
+            Use o botão abaixo para controlar a tranca
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center w-full">
+          <div className="flex flex-col gap-4 items-center justify-center w-88 md:w-120 p-12 bg-white rounded-lg shadow-md border border-gray-200">
+            <button className="cursor-pointer bg-foreground text-white p-8 rounded-full flex flex-col gap-4 items-center justify-center transition duration-200 hover:scale-105 active:scale-105">
+              <IoLockClosedOutline className="w-16 h-16" />
+              <span className="inline text-xl font-bold">Abrir tranca</span>
             </button>
-            <p className="mt-6 text-gray-600 text-sm">
-              Toque no botão para abrir a tranca
-            </p>
-            <p className="text-gray-500 text-sm">
-               A operação será registrada no histórico
-            </p>
-          </div>
-          <div className="p-4 w-full flex items-start justify-center flex-col gap-2 bg-white rounded-lg shadow-md border border-gray-200">
-            <div className="flex gap-2 items-center">
-              <FaRegClock className="w-6 h-6 text-foreground font-bold" />
-              <p className="text-2xl font-bold">Operações Recentes</p>
+
+            <div className="text-gray-600 text-sm  w-full text-center">
+              <p className="md:text-lg">
+                Toque no botão para abrir a tranca <br /> A opreção será
+                resgistrada no histórico
+              </p>
             </div>
-            <div className="text-gray-600 text-lg">
-              <p>Suas últimas operações</p>
-            </div>
-            <HistoryCard />
           </div>
+        </div>
+      </section>
+      <section className="p-4">
+        <div className="p-4 w-full flex items-start justify-center flex-col gap-2 bg-white rounded-lg shadow-md border border-gray-200">
+          <div className="flex gap-2 items-center">
+            <FaRegClock className="w-4 h-4 sm:w-6 sm:h-6 text-foreground font-bold" />
+            <p className="text-lg sm:text-2xl font-bold">Operações Recentes</p>
+          </div>
+          <div className="text-gray-600 text-base sm:text-lg">
+            <p>Suas últimas operações</p>
+          </div>
+          <HistoryCard />
         </div>
       </section>
     </div>
