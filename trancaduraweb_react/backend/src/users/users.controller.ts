c0aed08466170786f451a336af2fc82b92e21963
Genisson Emilio <esgenisson@gmail.com>
2025-06-15 15:13:38 -0300

    fazendo configuração para teste do backend

--- trancaduraweb_react/backend/src/users/users.controller.ts
+++ trancaduraweb_react/backend/src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() body: {
+    email: string;
+    username: string;
+    password: string;
+    firstName?: string;
+    lastName?: string;
+  }): Promise<User> {
+    return this.usersService.create(body);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User | null> {
+    return this.usersService.findById(id);
+  }
+}
--- trancaduraweb_react/backend/src/users/users.service.ts
+++ trancaduraweb_react/backend/src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  // Criar usuário
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  // Listar todos usuários
+  async findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  // Buscar usuário por id
+  async findById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}
