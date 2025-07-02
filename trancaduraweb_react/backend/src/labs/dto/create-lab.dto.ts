c2180b00d1877d128a8715f82f4d7a184a241fac
Genisson Emilio <esgenisson@gmail.com>
2025-07-02 16:04:28 -0300

    Add optional deviceId to CreateLabDto
    Introduced an optional deviceId property to the CreateLabDto class, validated as an integer. This allows associating a device with a lab during creation if desired.

--- trancaduraweb_react/backend/src/labs/dto/create-lab.dto.ts
+++ trancaduraweb_react/backend/src/labs/dto/create-lab.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from 'class-validator';
+// src/labs/dto/create-lab.dto.ts
+import { IsOptional, IsInt, IsString } from 'class-validator';
 
 export class CreateLabDto {
   @IsString()
   name: string;
+
+  @IsOptional()
+  @IsInt()
+  deviceId?: number;
 }
