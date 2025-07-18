089b9830b7625b0bcb65106318e115e4e999cb19
victor <vicfla1981@gmail.com>
2025-07-18 13:24:01 -0300

    test iot

--- trancaduraweb_react/backend/src/buttons/dto/create-button-device.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateButtonDeviceDto {
-  @IsString()
-  buttonId: string;
-
-  @IsString()
-  deviceId: string;
-}
--- trancaduraweb_react/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/api.ts
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  withCredentials: true,
-});
-
-export default api;
